80eaf10a155e25d7193bd9f752413c1a95af2a95
Ravindramandada <96417132+Ravindramandada@users.noreply.github.com>
2022-01-12 19:00:55 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-  echo "<h1><center><u>Welcome to Demo PHP Application</u></h1></center></br>";
+  echo "<h1><center><u>Welcome to Demo PHP Application on ECS </u></h1></center></br>";
   echo "<h2><center>Running PHP with Apache on Docker</h2></center>";
 "</br>";
 phpinfo();
